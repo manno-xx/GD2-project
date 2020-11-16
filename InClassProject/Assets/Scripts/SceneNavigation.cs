4f41b1aca9f8befca77f87a9ad425f7d4626a7d0
Manno <manno@xs4all.nl>
2020-11-16 21:05:33 +0100

    Added a menu with the navigation to the game scene closes #1

---
+++ InClassProject/Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class that implements basic navigation methods
+/// </summary>
+public class SceneNavigation : MonoBehaviour
+{
+
+    /// <summary>
+    /// Let the scene manager load a new scene
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to load</param>
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

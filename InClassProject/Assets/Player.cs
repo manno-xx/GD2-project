c8c561ffcaf86f34fd9e2468e21b23c185d86c66
Manno <manno@xs4all.nl>
2020-11-29 12:17:20 +0100

    Scene created, Player movement added

---
+++ InClassProject/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Crude way to move the player for now.
+/// No Rigid body involved etc.
+/// </summary>
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+
+    void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            0,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
